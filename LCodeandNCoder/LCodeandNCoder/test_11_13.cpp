67386480834b03690db7f0d9ebd50c46a97d3465
yhf869252662 <yhf753951@163.com>
2019-11-03 17:59:42 +0800

    把二叉搜索树转换为累加树

--- LCodeandNCoder/LCodeandNCoder/test_11_13.cpp
+++ LCodeandNCoder/LCodeandNCoder/test_11_13.cpp
@@ -1,28 +1,55 @@
 #include "commonly.h"
 
+//283 移动0
+//class Solution {
+//public:
+//	void moveZeroes(vector<int>& nums) {
+//		//STL使用
+//		auto it = nums.begin();
+//		size_t count = nums.size();
+//		while (count--)
+//		{
+//			if (*it == 0)
+//			{
+//				it = nums.erase(it);
+//				nums.push_back(0);
+//			}
+//			else
+//				++it;
+//		}
+//	}
+//
+//	void moveZeroes1(vector<int>& nums) {
+//		for (int last = 0, i = 0; i < nums.size(); ++i)
+//		{
+//			if (nums[i] != 0)
+//				swap(nums[last++], nums[i]);//last记录非0区间
+//		}
+//	}
+//};
+
+//538. 把二叉搜索树转换为累加树
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
 class Solution {
 public:
-	void moveZeroes(vector<int>& nums) {
-		//STL使用
-		auto it = nums.begin();
-		size_t count = nums.size();
-		while (count--)
-		{
-			if (*it == 0)
-			{
-				it = nums.erase(it);
-				nums.push_back(0);
-			}
-			else
-				++it;
-		}
+	int count = 0;
+	TreeNode* convertBST(TreeNode* root) {
+		if (root == NULL)
+			return root;
+
+		convertBST(root->right);
+		count += root->val;
+		root->val = count;
+		convertBST(root->left);
+
+		return root;
 	}
 
-	void moveZeroes1(vector<int>& nums) {
-		for (int last = 0, i = 0; i < nums.size(); ++i)
-		{
-			if (nums[i] != 0)
-				swap(nums[last++], nums[i]);//last记录非0区间
-		}
-	}
 };
